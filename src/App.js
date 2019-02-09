1837a46ebf87e1bf5035408c98b7da0b843ffad2
Dan Pasette <dan@10gen.com>
2019-02-09 14:55:44 -0500

    Playlists are loading

--- src/App.js
+++ src/App.js
@@ -13,54 +13,73 @@ const client = Stitch.initializeDefaultAppClient("best-fzcva");
 client.auth.loginWithCredential(new AnonymousCredential())
 const bestCol = client.getServiceClient(RemoteMongoClient.factory,
                                       "mongodb-atlas").db(dbName).collection(colName);
-let props = {client, bestCol};
+let props = {client: client, bestCol: bestCol};
 
 
 class Year extends Component {
 constructor(props) {
-   super(props);
+	super(props);
 
-   this.state = {
-      playlists: []
-   };
-   this.client = props.client;
-   this.bestCol = props.bestCol;
+	this.state = {
+		playlists: []
+	};
+	this.bestCol = props.bestCol;
+	this.year = null;
+}
+
+componentDidMount() {
+   this.loadPlaylist();
+}
+
+loadPlaylist() {
+  this.year = this.props.match.params.year;
+  const pipeline = [
+    { "$match": { "year": this.year } },
+    { "$group": { "_id": "$playlist" } },
+    { "$sort": {"_id": 1} }
+  ];
+  this.bestCol.aggregate(pipeline).asArray().then(docs => {
+    this.setState({ playlists: docs });
+  });
+}
+
+render() {
+	console.log("rendered playlists")
+   let result = (
+      <div>
+				<h3>Year: {this.year}</h3>
+				<ul style={{ listStyleType: "none", padding: 0 }}>
+        {this.state.playlists.map(playlist=> (
+          <li>
+             <div><Link to={`/year/${playlist._id}`}>{playlist._id}</Link></div>
+          </li>
+        ))}
+        </ul>
+      </div>
+   );
+   return result;
 }
 };
 
 
 class YearsList extends Component {
 constructor(props) {
-   super(props);
+  super(props);
 
-   this.state = {
-      years: []
-   };
-   this.client = props.client;
-   this.bestCol = props.bestCol;
+  this.state = {
+    years: []
+  };
+  this.bestCol = props.bestCol;
 }
 
 loadList() {
-  const pipeline = [ {
-      "$group": {
-        "_id": null,
-        "yearList": {
-          "$addToSet": {
-            "year": "$year"
-          }
-        }
-      }
-    },
-    { "$unwind": { "path": "$yearList"} },
-    { "$project": {"year": "$yearList.year"} }
+  const pipeline = [ 
+    { "$group": { "_id": "$year" } },
+    { "$sort": { "_id": 1 } }
   ];
   this.bestCol.aggregate(pipeline).asArray().then(docs => {
     this.setState({ years: docs });
   });
-}
-
-componentWillMount() {
-   this.loadList();
 }
 
 componentDidMount() {
@@ -68,17 +87,16 @@ componentDidMount() {
 }
 
 render() {
+	console.log("rendered years");
    let result = (
       <div>
-        <table>
+				<ul style={{ listStyleType: "none", padding: 0 }}>
         {this.state.years.map(year => (
-          <tr>
-          <td className="items-list">
-             <div><Link to={year.year}>{year.year}</Link></div>
-          </td>
-         </tr>
+          <li>
+             <div><Link to={`/year/${year._id}`}>{year._id}</Link></div>
+          </li>
         ))}
-        </table>
+        </ul>
       </div>
    );
    return result;
@@ -89,15 +107,33 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <b>The Best</b>
-          <YearsList {...props}/>
-        </header>
-      </div>
+				<div style={{ display: "flex" }}>
+					<div
+						style={{
+							padding: "10px",
+							width: "20%",
+							background: "#f0f0f0"
+						}}
+					>
+            <span className="logo">
+              <Link to="/" className="home-link">
+                The Best
+              </Link>
+            </span>
+						<YearsList {...props}/>
+					</div>
+					<div style={{ flex: 1, padding: "10px" }}>
+						<Route exact
+							path="/year/:year"
+							render={(props) => <Year {...props} bestCol={bestCol}/>}
+						/>
+						<div>
+						</div>
+          </div>
+       </div>
       </BrowserRouter>
-    );
-  }
+  );
 }
+};
 
 export default App;
