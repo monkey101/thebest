a75e034a66339ab02dd994b1dfcaa67513b41569
Dan Pasette <dan@mongodb.com>
2019-02-11 17:53:07 -0500

    Fixed search

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BSON } from 'mongodb-stitch';
 import {
      Stitch,
      RemoteMongoClient,
-    AnonymousCredential
+     AnonymousCredential
 } from "mongodb-stitch-browser-sdk";
 import './App.css';
 
@@ -19,13 +19,13 @@ let props = {client: client, bestCol: bestCol};
 
 class Year extends Component {
 constructor(props) {
-	super(props);
+  super(props);
 
-	this.state = {
-		playlists: []
-	};
-	this.bestCol = props.bestCol;
-	this.year = null;
+  this.state = {
+      playlists: []
+  };
+  this.bestCol = props.bestCol;
+  this.year = null;
 }
 
 componentDidUpdate(prevProps) {
@@ -35,11 +35,11 @@ componentDidUpdate(prevProps) {
 }
 
 componentDidMount() {
-   this.loadYear();
+  this.loadYear();
 }
 
 loadYear() {
-	console.log("loaded year")
+  console.log("loaded year")
   this.year = this.props.match.params.year;
   const pipeline = [
     { "$match": { "year": this.year } },
@@ -52,37 +52,37 @@ loadYear() {
 }
 
 render() {
-	console.log("rendered year")
-   let result = (
-      <div>
-				<h3>Year: {this.year}</h3>
-				<ul style={{ listStyleType: "none", padding: 0 }}>
+  console.log("rendered year")
+  let result = (
+    <div>
+      <h3>Year: {this.year}</h3>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
         {this.state.playlists.map(playlist=> (
-          <li key={playlist._id}>
+         <li key={playlist._id}>
              <div><Link to={`/year/${this.year}/${playlist._id}`}>{playlist._id}</Link></div>
-          </li>
+         </li>
         ))}
-        </ul>
-      </div>
-   );
-   return result;
+      </ul>
+    </div>
+  );
+  return result;
 }
 };
 
 class Playlist extends Component {
 constructor(props) {
-	super(props);
+   super(props);
 
-	this.state = {
-		tracks: []
-	};
-	this.bestCol = props.bestCol;
-	this.year = null;
-	this.playlist = null;
+   this.state = {
+       tracks: []
+   };
+   this.bestCol = props.bestCol;
+   this.year = null;
+   this.playlist = null;
 }
 
 componentDidMount() {
-   this.loadPlaylist();
+  this.loadPlaylist();
 }
 
 componentDidUpdate(prevProps) {
@@ -92,7 +92,7 @@ componentDidUpdate(prevProps) {
 }
 
 loadPlaylist() {
-	console.log("loaded playlist")
+  console.log("loaded playlist")
   this.year = this.props.match.params.year;
   this.playlist = this.props.match.params.playlist;
   const filter= { "year": this.year, "playlist": this.playlist};
@@ -102,33 +102,33 @@ loadPlaylist() {
 }
 
 render() {
-	console.log("rendered playlist")
-   let result = (
-      <div>
-				<h3>Playlist: {this.playlist}</h3>
-        <table border="1">
-          <tbody>
-            <tr>
-              <th>Artist</th>
-              <th>Track</th>
-              <th>Album</th>
-              <th>Genre</th>
-              <th>Time</th>
-            </tr>
-            {this.state.tracks.map(track => (
-            <tr key={track._id}>
-              <td>{track.artist}</td>
-              <td>{track.track}</td>
-              <td>{track.album}</td>
-              <td>{track.genre}</td>
-              <td>{track.time}</td>
-            </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-   );
-   return result;
+  console.log("rendered playlist")
+  let result = (
+    <div>
+      <h3>Playlist: {this.playlist}</h3>
+      <table border="1">
+        <tbody>
+          <tr>
+            <th>Artist</th>
+             <th>Track</th>
+             <th>Album</th>
+             <th>Genre</th>
+             <th>Time</th>
+           </tr>
+           {this.state.tracks.map(track => (
+           <tr key={track._id}>
+             <td>{track.artist}</td>
+             <td>{track.track}</td>
+             <td>{track.album}</td>
+             <td>{track.genre}</td>
+             <td>{track.time}</td>
+           </tr>
+           ))}
+        </tbody>
+      </table>
+    </div>
+  );
+  return result;
 }
 };
 
@@ -144,7 +144,7 @@ constructor(props) {
 }
 
 loadList() {
-	console.log("loaded years");
+  console.log("loaded years");
   const pipeline = [
     { "$group": { "_id": "$year" } },
     { "$sort": { "_id": 1 } }
@@ -155,23 +155,23 @@ loadList() {
 }
 
 componentDidMount() {
-   this.loadList();
+  this.loadList();
 }
 
 render() {
-	console.log("rendered years");
-   let result = (
-      <div>
-				<ul style={{ listStyleType: "none", padding: 0 }}>
-        {this.state.years.map(year => (
-          <li key={year._id}>
-             <div><Link to={`/year/${year._id}`}>{year._id}</Link></div>
-          </li>
-        ))}
-        </ul>
-      </div>
-   );
-   return result;
+  console.log("rendered years");
+  let result = (
+    <div>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+      {this.state.years.map(year => (
+        <li key={year._id}>
+          <div><Link to={`/year/${year._id}`}>{year._id}</Link></div>
+        </li>
+      ))}
+      </ul>
+    </div>
+  );
+  return result;
 }
 };
 
@@ -186,54 +186,58 @@ constructor(props) {
 }
 
 componentDidMount() {
-   this.input.focus();
+  this.input.focus();
 }
 
 search(e) {
-	const s = this.input.value;
-	const filter = { $or: [{ "artist": BSON.BSONRegExp(s, 'i') }, { "track": BSON.BSONRegExp(s, 'i') }]};
+  const s = this.input.value;
+  const regex = BSON.BSONRegExp(s, "i")
+  const filter = { $or: [
+    { artist: { "$regularExpression":{"pattern": s,"options": "i"}}},
+    { track : { "$regularExpression":{"pattern": s,"options": "i"}}}
+  ]};
 
   console.log(filter)
-	const options = {
-		sort: { track: 1 },
-		limit: 30,
-	};
+  const options = {
+    sort: { track: 1 },
+    limit: 30,
+  };
 
-	this.bestCol.find(filter, options).asArray().then(results => {
-      this.setState({ matches: results });
-	});
+  this.bestCol.find(filter, options).asArray().then(results => {
+    this.setState({ matches: results });
+  });
 }
 
 
 render() {
-	return (
-	<div className="content">
-		<div>
-			Search For<input
-					ref={input => {
-						this.input = input;
-					}}
-					id="search_string"
-					onChange={() => this.search()}
-			/>
-		</div>
-		<table border="1">
-			<tbody>
-				<tr>
-					<th>Track</th>
-					<th>Artist</th>
-				</tr>
+  return (
+  <div className="content">
+    <div>
+      Search For <input
+        ref={input => {
+           this.input = input;
+        }}
+        id="search_string"
+        onChange={() => this.search()}
+      />
+    </div>
+      <table border="1">
+        <tbody>
+          <tr>
+            <th>Track</th>
+            <th>Artist</th>
+          </tr>
 
-				{this.state.matches.map(p => (
-				<tr key={p._id}>
-					<td>{p.track}</td>
-					<td>{p.artist}</td>
-				</tr>
-				))}
-			</tbody>
-		</table>
-	</div>
-	);
+          {this.state.matches.map(p => (
+          <tr key={p._id}>
+            <td>{p.track}</td>
+            <td>{p.artist}</td>
+          </tr>
+          ))}
+       </tbody>
+     </table>
+   </div>
+  );
 }
 };
 
@@ -241,46 +245,46 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-				<div style={{ display: "flex" }}>
-					<div
-						style={{
-							padding: "10px",
-							width: "10%",
-							background: "#f0f0f0"
-						}}
-					>
-            <span className="logo">
-              <Link to="/" className="home-link">
-                <h3>The Best</h3>
-              </Link>
-              <Link to="/search" className="home-link">
-                <h3>Search</h3>
-              </Link>
-            </span>
-						<YearsList {...props}/>
-					</div>
-					<div style={{ flex: 1, width: "20%", padding: "10px" }}>
-						<Route
-              exact
-							path="/search"
-							render={(props) => <Search {...props} bestCol={bestCol}/>}
-						/>
-          </div>
-					<div style={{ flex: 1, width: "20%", padding: "10px" }}>
-						<Route
-							path="/year/:year"
-							render={(props) => <Year {...props} bestCol={bestCol}/>}
-						/>
-          </div>
-					<div style={{ flex: 3, width: "80%", padding: "10px" }}>
-						<Route
-              exact
-							path="/year/:year/:playlist"
-							render={(props) => <Playlist {...props} bestCol={bestCol}/>}
-						/>
-          </div>
-       </div>
-      </BrowserRouter>
+        <div style={{ display: "flex" }}>
+          <div
+            style={{
+              padding: "10px",
+              width: "10%",
+              background: "#f0f0f0"
+            }}
+          >
+          <span className="logo">
+            <Link to="/" className="home-link">
+              <h3>The Best</h3>
+            </Link>
+            <Link to="/search" className="home-link">
+              <h3>Search</h3>
+            </Link>
+          </span>
+          <YearsList {...props}/>
+        </div>
+        <div style={{ flex: 1, width: "20%", padding: "10px" }}>
+          <Route
+            exact
+            path="/search"
+            render={(props) => <Search {...props} bestCol={bestCol}/>}
+          />
+        </div>
+        <div style={{ flex: 1, width: "20%", padding: "10px" }}>
+          <Route
+            path="/year/:year"
+            render={(props) => <Year {...props} bestCol={bestCol}/>}
+          />
+        </div>
+        <div style={{ flex: 3, width: "80%", padding: "10px" }}>
+          <Route
+            exact
+            path="/year/:year/:playlist"
+            render={(props) => <Playlist {...props} bestCol={bestCol}/>}
+          />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 };
