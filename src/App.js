34358ea15dec52271bc85465da695af643a40fe4
Dan Pasette <dan@10gen.com>
2019-02-10 22:11:49 -0500

    Added playlist tracks

--- src/App.js
+++ src/App.js
@@ -27,11 +27,18 @@ constructor(props) {
 	this.year = null;
 }
 
-componentDidMount() {
-   this.loadPlaylist();
+componentDidUpdate(prevProps) {
+  if (this.props.match.params.year !== prevProps.match.params.year) {
+    this.loadYear();
+  }
 }
 
-loadPlaylist() {
+componentDidMount() {
+   this.loadYear();
+}
+
+loadYear() {
+	console.log("loaded year")
   this.year = this.props.match.params.year;
   const pipeline = [
     { "$match": { "year": this.year } },
@@ -44,17 +51,80 @@ loadPlaylist() {
 }
 
 render() {
-	console.log("rendered playlists")
+	console.log("rendered year")
    let result = (
       <div>
 				<h3>Year: {this.year}</h3>
 				<ul style={{ listStyleType: "none", padding: 0 }}>
         {this.state.playlists.map(playlist=> (
-          <li>
-             <div><Link to={`/year/${playlist._id}`}>{playlist._id}</Link></div>
+          <li key={playlist._id}>
+             <div><Link to={`/year/${this.year}/${playlist._id}`}>{playlist._id}</Link></div>
           </li>
         ))}
         </ul>
+      </div>
+   );
+   return result;
+}
+};
+
+class Playlist extends Component {
+constructor(props) {
+	super(props);
+
+	this.state = {
+		tracks: []
+	};
+	this.bestCol = props.bestCol;
+	this.year = null;
+	this.playlist = null;
+}
+
+componentDidMount() {
+   this.loadPlaylist();
+}
+
+componentDidUpdate(prevProps) {
+  if (this.props.match.params.playlist !== prevProps.match.params.playlist) {
+    this.loadPlaylist();
+  }
+}
+
+loadPlaylist() {
+	console.log("loaded playlist")
+  this.year = this.props.match.params.year;
+  this.playlist = this.props.match.params.playlist;
+  const filter= { "year": this.year, "playlist": this.playlist};
+  this.bestCol.find(filter).asArray().then(docs => {
+    this.setState({ tracks: docs });
+  });
+}
+
+render() {
+	console.log("rendered playlist")
+   let result = (
+      <div>
+				<h3>Playlist: {this.playlist}</h3>
+        <table border="1">
+          <tbody>
+            <tr>
+              <th>Artist</th>
+              <th>Track</th>
+              <th>Album</th>
+              <th>Genre</th>
+              <th>Time</th>
+            </tr>
+            {this.state.tracks.map(track => (
+            <tr key={track._id}>
+              <td>{track.artist}</td>
+              <td>{track.track}</td>
+              <td>{track.album}</td>
+              <td>{track.genre}</td>
+              <td>{track.time}</td>
+            </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
    );
    return result;
@@ -73,7 +143,8 @@ constructor(props) {
 }
 
 loadList() {
-  const pipeline = [ 
+	console.log("loaded years");
+  const pipeline = [
     { "$group": { "_id": "$year" } },
     { "$sort": { "_id": 1 } }
   ];
@@ -92,7 +163,7 @@ render() {
       <div>
 				<ul style={{ listStyleType: "none", padding: 0 }}>
         {this.state.years.map(year => (
-          <li>
+          <li key={year._id}>
              <div><Link to={`/year/${year._id}`}>{year._id}</Link></div>
           </li>
         ))}
@@ -111,7 +182,7 @@ class App extends Component {
 					<div
 						style={{
 							padding: "10px",
-							width: "20%",
+							width: "10%",
 							background: "#f0f0f0"
 						}}
 					>
@@ -122,13 +193,17 @@ class App extends Component {
             </span>
 						<YearsList {...props}/>
 					</div>
-					<div style={{ flex: 1, padding: "10px" }}>
-						<Route exact
+					<div style={{ flex: 1, width: "20%", padding: "10px" }}>
+						<Route
 							path="/year/:year"
 							render={(props) => <Year {...props} bestCol={bestCol}/>}
 						/>
-						<div>
-						</div>
+          </div>
+					<div style={{ flex: 3, width: "80%", padding: "10px" }}>
+						<Route
+							path="/year/:year/:playlist"
+							render={(props) => <Playlist {...props} bestCol={bestCol}/>}
+						/>
           </div>
        </div>
       </BrowserRouter>
