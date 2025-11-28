ccc6fc6d65027511b2ceb7931ab51da838e3f94c
Dan Pasette <monkey101@gmail.com>
2025-11-28 12:05:41 +0100

    Added clickable spotify link.

--- public/script.js
+++ public/script.js
@@ -118,10 +118,21 @@ function createPlaylistElement(playlist) {
 
       html += `
         <div class="track-item">
-          <p class="track-artist">
-            <a href="#" class="artist-link" data-artist="${escapeHtml(track.artist)}">${escapeHtml(track.artist)}</a>
-          </p>
-          <p><strong>${escapeHtml(track.track)}</strong> - <a href="#" class="album-link" data-album="${escapeHtml(track.album)}">${escapeHtml(track.album)}</a></p>
+          <h3>
+            ${escapeHtml(track.track)}
+            <a href="https://open.spotify.com/search/${encodeURIComponent(track.track + ' ' + track.artist)}"
+               target="_blank"
+               class="spotify-link"
+               title="Play on Spotify">
+              <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+                <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z"/>
+              </svg>
+            </a>
+          </h3>
+          <div class="playlist-meta">
+            <span><strong>Artist:</strong> <a href="#" class="artist-link" data-artist="${escapeHtml(track.artist)}">${escapeHtml(track.artist)}</a></span>
+            <span><strong>Album:</strong> <a href="#" class="album-link" data-album="${escapeHtml(track.album)}">${escapeHtml(track.album)}</a></span>
+          </div>
           <p><small>Genre: ${track.genre || 'N/A'} | Duration: ${track.time || 'N/A'}</small></p>
         </div>
       `;
@@ -351,7 +362,17 @@ function createSearchResultElement(result) {
   const div = document.createElement('div');
   div.className = 'search-result-item';
    let html = `
-    <h3>${escapeHtml(result.track)}</h3>
+    <h3>
+      ${escapeHtml(result.track)}
+      <a href="https://open.spotify.com/search/${encodeURIComponent(result.track + ' ' + result.artist)}"
+         target="_blank"
+         class="spotify-link"
+         title="Play on Spotify">
+        <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
+          <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z"/>
+        </svg>
+      </a>
+    </h3>
     <div class="playlist-meta">
       <span><strong>Artist:</strong> <a href="#" class="artist-link" data-artist="${escapeHtml(result.artist)}">${escapeHtml(result.artist)}</a></span>
       <span><strong>Album:</strong> <a href="#" class="album-link" data-album="${escapeHtml(result.album)}">${escapeHtml(result.album)}</a></span>
